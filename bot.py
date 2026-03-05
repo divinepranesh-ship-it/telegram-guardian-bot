ba02d67caf1edb348504c88012342faabe974f52
divinepranesh-ship-it <divinepranesh@gmail.com>
2026-03-05 10:19:31 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,40 @@
+import logging
+from telegram.ext import ApplicationBuilder, MessageHandler, CommandHandler, filters
+import config
+import filters as msgfilter
+import admin
+
+logging.basicConfig(level=logging.INFO)
+
+async def start(update, context):
+
+    await update.message.reply_text(
+        "🛡 Guardian Bot Active\nGroup protected from spam."
+    )
+
+
+async def message_handler(update, context):
+
+    if update.message.text:
+        await msgfilter.check_message(update, context)
+
+
+def main():
+
+    app = ApplicationBuilder().token(config.TOKEN).build()
+
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(CommandHandler("ban", admin.ban))
+    app.add_handler(CommandHandler("mute", admin.mute))
+
+    app.add_handler(
+        MessageHandler(filters.TEXT & (~filters.COMMAND), message_handler)
+    )
+
+    print("Guardian bot running...")
+
+    app.run_polling()
+
+
+if __name__ == "__main__":
+    main()
