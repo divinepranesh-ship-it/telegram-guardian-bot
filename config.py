7612206b0dc400d86b1c0bfed5cd5f89b9363e70
divinepranesh-ship-it <divinepranesh@gmail.com>
2026-03-05 10:18:25 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,18 @@
+TOKEN = "8783119872:AAEhWqeQi-WBeNMq3WexW7rP1HmvFXwABow"
+
+MAX_WARNINGS = 3
+SPAM_LIMIT = 5
+TIME_WINDOW = 10
+
+BLOCK_LINKS = True
+
+BLOCKED_LINKS = [
+"t.me/",
+"http://",
+"https://"
+]
+
+BAD_WORDS = [
+"porn","xxx","sex",
+"தேவடியா","புண்டை","ஓம்பு"
+]
